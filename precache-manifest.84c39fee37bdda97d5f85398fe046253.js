18e79dde422c97931bee49b351b08456fb94af0b
ujalakhurram <ujala.khurram@venturedive.com>
2021-06-30 11:16:16 +0500

    Updates

---
+++ precache-manifest.84c39fee37bdda97d5f85398fe046253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c1d92012b1b1d947f8cfc5ce9002ba",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "d987c8805aa94073e043",
+    "url": "/color-generator/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "020f32d819c2fa5bb0e5",
+    "url": "/color-generator/static/js/2.0b6c26cf.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator/static/js/2.0b6c26cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d987c8805aa94073e043",
+    "url": "/color-generator/static/js/main.18b5ff03.chunk.js"
+  },
+  {
+    "revision": "a1cfb88a1dc1d9e37dd7",
+    "url": "/color-generator/static/js/runtime-main.8a33e40c.js"
+  }
+]);
